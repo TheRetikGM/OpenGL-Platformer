40218165c6c2582a519308c8c0749ec24bac8abd
TheRetikGM <theretikgm@gmail.com>
2021-09-09 20:02:33 +0200

    pridani testovacich collision objektu

--- assets/tilemaps/test/environment.tsx
+++ assets/tilemaps/test/environment.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.1" name="environment" tilewidth="16" tileheight="16" tilecount="72" columns="12">
+<tileset version="1.5" tiledversion="1.7.2" name="environment" tilewidth="16" tileheight="16" tilecount="72" columns="12">
  <image source="tileset.png" width="192" height="96"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
@@ -8,39 +8,35 @@
  </tile>
  <tile id="1">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="1" x="0.272727" y="0.0909091" width="15.9091" height="16.0909"/>
   </objectgroup>
  </tile>
  <tile id="2">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
- </tile>
- <tile id="13">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="2.72007" y="2.27294">
-    <polygon points="0,0 5.92455,12.7061 11.9609,-3.53982"/>
+   <object id="1" x="8" y="8.54545">
+    <point/>
    </object>
   </objectgroup>
  </tile>
- <tile id="28">
+ <tile id="12">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="1" x="2.81818" y="1.72727" width="8.18182" height="13.4545">
+    <ellipse/>
+   </object>
   </objectgroup>
  </tile>
- <tile id="42">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="16" height="16"/>
+ <tile id="13">
+  <objectgroup draworder="index" id="3">
+   <object id="3" x="7.90909" y="7.45455">
+    <ellipse/>
+   </object>
   </objectgroup>
  </tile>
- <tile id="43">
+ <tile id="14">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
- </tile>
- <tile id="44">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="1" x="3.81818" y="3">
+    <polygon points="0,0 -1.36364,7.63636 7.27273,12.2727 11.1818,-0.0909091 5.54545,-1.90909"/>
+   </object>
   </objectgroup>
  </tile>
  <wangsets>
